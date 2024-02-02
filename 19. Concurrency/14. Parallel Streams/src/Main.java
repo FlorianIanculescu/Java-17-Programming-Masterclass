cd24bf30b5aa59f29110730449e42bb6c2c3e091
Florian-Cristinel Ianculescu <florian.ianculescu@gmail.com>
2024-02-02 09:26:27 +0200

    Add files via upload

---
+++ 19. Concurrency/14. Parallel Streams/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int numbersLength = 100_000_000;
+        long[] numbers = new Random().longs(numbersLength, 1, numbersLength).toArray();
+
+        long delta = 0;
+        int iterations = 25;
+
+        for (int i = 0; i < iterations; i++) {
+            long start = System.nanoTime();
+            double averageSerial = Arrays.stream(numbers)
+                    .parallel()
+                    .average()
+                    .orElseThrow();
+
+            long elapsedSerial = System.nanoTime() - start;
+//            System.out.printf("Avg = %.2f, elapsed %d nanos or %.2f ms%n",
+//                    averageSerial, elapsedSerial, elapsedSerial / 1_000_000.0);
+
+            start = System.nanoTime();
+            double averageParallel = Arrays.stream(numbers).average().orElseThrow();
+            long elapsedParallel = System.nanoTime() - start;
+//            System.out.printf("Avg = %.2f, elapsed %d nanos or %.2f ms%n",
+//                    averageSerial, elapsedParallel, elapsedParallel / 1_000_000.0);
+
+            delta += (elapsedSerial - elapsedParallel);
+        }
+
+        System.out.printf("Parallel is [%d] nanos or [%.2f] ms faster on average %n",
+                delta / iterations, delta / 1_000_000.0 / iterations);
+    }
+}
